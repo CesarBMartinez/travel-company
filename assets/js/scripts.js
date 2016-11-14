ce4b614afca3e3b3b3754bff1d075338ee8a56cf
César Martinez <cesar_matinez22@hotmail.com>
2016-11-14 00:20:29 -0200

    README.md

---
+++ assets/js/scripts.js
@@ -0,0 +1,15 @@
+// Scripts //
+$("#form form").on("submit", function(){
+    var a = document.forms["form"]["name"].value;
+    var b = document.forms["form"]["email"].value;
+    var c = document.forms["form"]["phone"].value;
+    var d = document.forms["form"]["mensagem"].value;
+    if (a==null || a=="",b==null || b=="",c==null || c=="",d==null || d==""){
+	  	alert("Favor preencher todos os campos");
+    }else{
+		alert("Formulário enviado com sucesso");
+		$('#form form').trigger("reset");
+    }
+
+    return false;
+});
